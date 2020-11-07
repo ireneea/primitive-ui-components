2347b8ff8303ac9442e320dcc58dd3cc445a1119
irenee <irenee@qchi.net>
2020-11-07 15:42:18 +0000

    refactor: used a HOC for heading components

--- src/components/Headings.tsx
+++ src/components/Headings.tsx
@@ -1,89 +1,37 @@
-import styled, { css } from "styled-components";
+import styled, { AnyStyledComponent } from "styled-components";
 import theme from "./themes";
 import { baseFont } from "./mixins";
 
-// Headings
-// TODO: mobile
+function setHeader(level: number) {
+  return function withHeaderStyle(Header: AnyStyledComponent) {
+    const styledHeader = styled(Header)`
+      ${baseFont}
+      margin: ${(props) => props.theme.spacing.margins} 0;
+      font-weight: ${(props) => props.theme.headings.headingFontWeight};
+      font-family: ${(props) => props.theme.headings.headingFontFamily};
+      line-height: ${(props) => props.theme.headings.headingLineHeight};
+      font-size: ${(props) => props.theme.headings[`h${level}MobileSize`]};
 
-const heading = css`
-  ${baseFont}
-  margin: ${(props) => props.theme.spacing.margins} 0;
-  font-weight: ${(props) => props.theme.headings.headingFontWeight};
-  font-family: ${(props) => props.theme.headings.headingFontFamily};
-  line-height: ${(props) => props.theme.headings.headingLineHeight};
-`;
+      &:not(:first-child) {
+        margin: 1.5rem 0;
+      }
 
-export const H1 = styled.h1`
-  ${heading}
-  font-size: ${(props) => props.theme.headings.h1MobileSize};
-  &:not(:first-child) {
-    margin-top: 2rem;
-  }
+      @media ${(props) => props.theme.mobileBreakPoint} {
+        font-size: ${(props) => props.theme.headings[`h${level}Size`]};
+      }
+    `;
 
-  @media ${(props) => props.theme.mobileBreakPoint} {
-    font-size: ${(props) => props.theme.headings.h1Size};
-    &:not(:first-child) {
-      margin-top: 2.5rem;
-    }
-  }
-`;
+    styledHeader.defaultProps = { theme: { ...theme } };
+    return styledHeader;
+  };
+}
 
-export const H2 = styled.h2`
-  ${heading}
-  font-size: ${(props) => props.theme.headings.h2MobileSize};
-  &:not(:first-child) {
-    margin-top: 2rem;
-  }
+export const H1 = setHeader(1)(styled.h1``);
 
-  @media ${(props) => props.theme.mobileBreakPoint} {
-    font-size: ${(props) => props.theme.headings.h2Size};
-    &:not(:first-child) {
-      margin-top: 2.5rem;
-    }
-  }
-`;
+export const H2 = setHeader(2)(styled.h2``);
 
-export const H3 = styled.h3`
-  ${heading}
-  font-size: ${(props) => props.theme.headings.h3MobileSize};
-  &:not(:first-child) {
-    margin-top: 2rem;
-  }
+export const H3 = setHeader(3)(styled.h3``);
 
-  @media ${(props) => props.theme.mobileBreakPoint} {
-    font-size: ${(props) => props.theme.headings.h3Size};
-    &:not(:first-child) {
-      margin-top: 2.5rem;
-    }
-  }
-`;
+export const H4 = setHeader(4)(styled.h4``);
 
-export const H4 = styled.h4`
-  ${heading}
-  font-size: ${(props) => props.theme.headings.h4MobileSize};
-  &:not(:first-child) {
-    margin: 1.5rem 0;
-  }
-
-  @media ${(props) => props.theme.mobileBreakPoint} {
-    font-size: ${(props) => props.theme.headings.h4Size};
-  }
-`;
-
-export const H5 = styled.h5`
-  ${heading}
-  font-size: ${(props) => props.theme.headings.h5MobileSize};
-  &:not(:first-child) {
-    margin: 1.5rem 0;
-  }
-
-  @media ${(props) => props.theme.mobileBreakPoint} {
-    font-size: ${(props) => props.theme.headings.h5Size};
-  }
-`;
-
-H1.defaultProps = { theme: { ...theme } };
-H2.defaultProps = { theme: { ...theme } };
-H3.defaultProps = { theme: { ...theme } };
-H4.defaultProps = { theme: { ...theme } };
-H5.defaultProps = { theme: { ...theme } };
+export const H5 = setHeader(5)(styled.h5``);
