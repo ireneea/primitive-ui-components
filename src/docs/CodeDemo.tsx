98d0032ab8e3de83e2b7221269ba12647a2c7526
irenee <irenee@qchi.net>
2020-11-21 13:00:22 +0000

    feat: Code Demo fix

--- src/docs/CodeDemo.tsx
+++ src/docs/CodeDemo.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { JsxCode } from "./JsxCode";
 
 import { DocSection } from "./DocSection";
-import { CodeBlock } from "Primitive";
 
 const snippet = `
 import { GlobalStyle } from 'Primitive'
@@ -11,14 +10,13 @@ import { GlobalStyle } from 'Primitive'
 const App = () => (
   <>
     <CodeBlock>
-    ${`
-const a = 10;
-console.log(a);`}
+    ${`$\{\`const a = 10;
+console.log(a);\`}`}
     </ CodeBlock>
-    <Heading level={2} />
-    <Heading level={3} />
-    <Heading level={4} />
-    <Heading level={5} />
+
+    <InlineCode>a</InlineCode> is a variable
+
+    <KeyboardCmd>Ctrl + v</KeyboardCmd>
   </>
 );
 `;
